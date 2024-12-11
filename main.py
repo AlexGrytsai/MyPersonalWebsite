3a644448c876819f97e05da0356962cb9083f5b1
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-11 15:02:15 +0200

    Changed {...} to value info in def me

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 
-from app.my_data import info_json
-
+from app.my_data import info_json, info
 
 description = f"""
 ### Welcome to my professional site, which is styled like API documentation.**
@@ -56,31 +55,7 @@ async def redoc_docs():
                 "technical skills, and biography.",
 )
 async def me():
-    return {
-        "name": "Oleksandr Grytsai",
-        "profession": "Python Developer",
-        "skills": [
-            "Python", "Django", "Django REST Framework", "Docker",
-            "Docker Compose", "Asynchronous Python",
-            "SQL", "PostgreSQL", "Redis", "Azure", "Google Cloud", "AWS",
-            "GitHub Actions", "CI/CD"
-        ],
-        "bio": (
-            "I am a Python developer with specializing in Python, Django,"
-            "the Django REST Framework (DRF), and Docker.\n"
-            "I have expertise in PostgreSQL, Redis, and Azure SQL databases, "
-            "as well as working with Azure, GCP and AWS cloud services.\n"
-            "Before transitioning to IT, I worked as an engineer "
-            "designing engineering networks and as a project group leader, "
-            "where I developed strong organizational and communication skills."
-            " This experience honed my ability to solve complex problems, "
-            "manage teamwork, and make independent decisions."
-        ),
-        "languages": {
-            "English": "Intermediate",
-            "Ukrainian": "Native"
-        },
-    }
+    return info
 
 
 @app.get(
