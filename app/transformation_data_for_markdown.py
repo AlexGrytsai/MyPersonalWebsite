aeaf42f39d8347a2b18730694cae2aa9fa2f66c6
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-17 11:31:07 +0200

    created transformation_data_for_markdown.py with MarkdownTransformer

---
+++ app/transformation_data_for_markdown.py
@@ -0,0 +1,61 @@
+from app.my_data import (
+    cloud_services,
+    programming_languages,
+    frameworks,
+    db,
+    container,
+    vcs,
+)
+
+
+class MarkdownTransformer:
+    """
+    A class used to transform data into Markdown format.
+
+    This class provides static methods to transform different types
+    of data into Markdown format.
+    """
+
+    @staticmethod
+    def programming_languages_section() -> str:
+        return "".join(
+            f"{language['badge']}\n" for language in programming_languages
+        )
+
+    @staticmethod
+    def frameworks_section() -> str:
+        return "".join(f"{framework['badge']}\n" for framework in frameworks)
+
+    @staticmethod
+    def data_bases_section() -> str:
+        return "".join(f"{base['badge']}\n" for base in db)
+
+    @staticmethod
+    def container_section() -> str:
+        return "\n".join(
+            f"{container_item['badge']}" for container_item in container
+        )
+
+    @staticmethod
+    def vcs_section() -> str:
+        return "\n".join(f"{vcs_item['badge']}" for vcs_item in vcs)
+
+    @staticmethod
+    def cloud_services_section() -> str:
+        cloud_services_list = []
+        for cloud in cloud_services:
+            cloud_logo = (
+                f'<img src = "{cloud["logo_url"]}" alt="{cloud["name"]}" '
+                f'width="50"> **{cloud["name"]}**\n'
+            )
+            for service in cloud["services"]:
+                service_logo = (
+                    f'<img src = "{service["logo_url"]}" '
+                    f'alt="{service["name"]}" width ="30">'
+                )
+                cloud_logo += f"+ {service_logo} {service['name']}\n"
+            cloud_services_list.append(cloud_logo)
+            cloud_services_list.append("---")
+            cloud_services_list.append("\n")
+
+        return "\n".join(cloud_services_list)
